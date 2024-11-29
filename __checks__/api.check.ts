007070df54d8c16407ca258a975a9ef99c9ef97a
schobele <leoschoberwalter@gmail.com>
2024-11-29 15:45:05 +0100

    Add JSON body length assertion to API check

--- __checks__/api.check.ts
+++ __checks__/api.check.ts
@@ -1,24 +1,23 @@
-import { ApiCheck, AssertionBuilder } from 'checkly/constructs'
+import { ApiCheck, AssertionBuilder } from "checkly/constructs";
 
-
-new ApiCheck('get-books-api-check', {
-  name: 'GET /books',
-  degradedResponseTime: 10000,
-  maxResponseTime: 20000,
-  request: {
-    url: 'https://danube-store-srebot-test-3a4a0227b89f.herokuapp.com/api/books',
-    method: 'GET',
-    headers: [
-      {
-        key: 'Accept',
-        value: 'application/json'
-      }
-    ],
-    followRedirects: true,
-    skipSSL: false,
-    assertions: [
-      AssertionBuilder.statusCode().equals(200),
-    ],
-  }
-})
-
+new ApiCheck("get-books-api-check", {
+	name: "GET /books",
+	degradedResponseTime: 10000,
+	maxResponseTime: 20000,
+	request: {
+		url: "https://danube-store-srebot-test-3a4a0227b89f.herokuapp.com/api/books",
+		method: "GET",
+		headers: [
+			{
+				key: "Accept",
+				value: "application/json",
+			},
+		],
+		followRedirects: true,
+		skipSSL: false,
+		assertions: [
+			AssertionBuilder.statusCode().equals(200),
+			AssertionBuilder.jsonBody("$.length").greaterThan(0),
+		],
+	},
+});
