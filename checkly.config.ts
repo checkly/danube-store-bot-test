7c504a89ee404523f592edda044fc29675e837d9
Daniel Paulus <daniel@checklyhq.com>
2024-11-29 15:27:32 +0100

    add second alert channel

--- checkly.config.ts
+++ checkly.config.ts
@@ -33,12 +33,44 @@ export const webhookChannel = new WebhookAlertChannel('webhook-239286', {
 }`,
 })
 
+export const webhookChannellocal = new WebhookAlertChannel('webhook-2392868', {
+  name: 'srebot-prod-webhook-local',
+  method: 'POST',
+  url: new URL('https://cd97-62-96-56-52.ngrok-free.app/checkly-webhook'),
+  sendRecovery: false,
+  sendFailure: false,
+  sendDegraded: false,
+  sslExpiry: false,
+  template: `{
+  "CHECK_NAME": "{{ CHECK_NAME }}",
+  "CHECK_ID": "{{ CHECK_ID }}",
+  "CHECK_TYPE": "{{ CHECK_TYPE }}",
+  "GROUP_NAME": "{{ GROUP_NAME }}",
+  "ALERT_TITLE": "{{ ALERT_TITLE }}",
+  "ALERT_TYPE": "{{ ALERT_TYPE }}",
+  "CHECK_RESULT_ID": "{{ CHECK_RESULT_ID }}",
+  "RESPONSE_TIME": "{{ RESPONSE_TIME }}",
+  "API_CHECK_RESPONSE_STATUS_CODE": "{{ API_CHECK_RESPONSE_STATUS_CODE }}",
+  "API_CHECK_RESPONSE_STATUS_TEXT": "{{ API_CHECK_RESPONSE_STATUS_TEXT }}",
+  "RUN_LOCATION": "{{ RUN_LOCATION }}",
+  "RESULT_LINK": "{{ RESULT_LINK }}",
+  "SSL_DAYS_REMAINING": "{{ SSL_DAYS_REMAINING }}",
+  "SSL_CHECK_DOMAIN": "{{ SSL_CHECK_DOMAIN }}",
+  "STARTED_AT": "{{ STARTED_AT }}",
+  "TAGS": "{{ TAGS }}",
+  "$RANDOM_NUMBER": "{{ $RANDOM_NUMBER }}",
+  "$UUID": "{{ $UUID }}",
+  "moment": "{{ moment }}"
+}`,
+})
+
+
 export default defineConfig({
  projectName: 'Website Monitoring',
  logicalId: 'website-monitoring-1',
  repoUrl: 'https://github.com/acme/website',
  checks: {
-  alertChannels: [webhookChannel],
+  alertChannels: [webhookChannel, webhookChannellocal],
    activated: true,
    muted: false,
    runtimeId: '2022.10',
