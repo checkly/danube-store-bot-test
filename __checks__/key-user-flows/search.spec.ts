6bd25727c7821531e066a388f3edf6faa3813e73
Jan Grzesik <jmgrzesik@gmail.com>
2025-01-15 15:31:57 +0100

    Test: change method to performSearches

--- __checks__/key-user-flows/search.spec.ts
+++ __checks__/key-user-flows/search.spec.ts
@@ -9,7 +9,7 @@ test('search', async ({page}) => {
     const shopPage = new DanubeShopPage(page);
     const itemDetailsPage = new DanubeItemDetailsPage(page);
 
-    await shopPage.performSearch('For')
+    await shopPage.performSearches('For')
     await shopPage.selectShopItem()
     expect(await itemDetailsPage.isActive())
 })
