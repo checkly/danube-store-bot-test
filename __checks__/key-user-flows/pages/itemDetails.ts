aa35c631183c9c1a9a5448ff50691c120bcbae6d
Jan Grzesik <jmgrzesik@gmail.com>
2025-01-27 16:06:16 +0100

    refactor: cleanup the item details page

--- __checks__/key-user-flows/pages/itemDetails.ts
+++ __checks__/key-user-flows/pages/itemDetails.ts
@@ -1,22 +1,21 @@
-import { expect, Locator, Page } from '@playwright/test';
+import { Locator, Page } from '@playwright/test';
 
 export class DanubeItemDetailsPage {
-	readonly page: Page;
-	readonly addToCartButton: Locator;
-	readonly detailsContainer: Locator;
+  readonly page: Page;
+  readonly addToCartButton: Locator;
+  readonly detailsContainer: Locator;
 
-	constructor(page: Page) {
-		this.page = page;
-		this.addToCartButton = page.locator('text="add to cart"');
-		this.detailsContainer = page.locator('.detail')
-	}
+  constructor(page: Page) {
+    this.page = page;
+    this.addToCartButton = page.locator('text="add to cart"');
+    this.detailsContainer = page.locator('.detail')
+  }
 
-	async addToCart() {
-		await this.addToCartButton.click();
-	}
+  async addToCart() {
+    await this.addToCartButton.click();
+  }
 
-	async isActive() {
-		return this.detailsContainer.isVisible();
-	}
-
+  async isActive() {
+    return this.detailsContainer.isVisible();
+  }
 }
